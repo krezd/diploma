0d6dac52cd2aae8177ea3e8792143a1156e88323
andreyasankin <andrey_asankin@mail.ru>
2026-01-18 17:38:22 +0300

    init

---
+++ backend/src/test/java/ru/krezd/diploma/DiplomaApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.krezd.diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
